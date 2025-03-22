2f480144cbff26060a60a1d21e9eed48482713b2
mahedy25 <mahedyhasan873@gmail.com>
2025-03-22 22:52:44 +0600

    some changes

--- components/ui/shared/header/menu.tsx
+++ components/ui/shared/header/menu.tsx
@@ -1,4 +1,4 @@
-import { ShoppingCartIcon, UserIcon } from "lucide-react"
+import { ShoppingCartIcon} from "lucide-react"
 import Link from "next/link"
 
 export default function Menu(){
@@ -6,14 +6,14 @@ export default function Menu(){
         <div className="flex justify-end">
             <nav className="flex gap-3 w-full">
 
-              <Link href='/signin' className="header-button">
-               <UserIcon className="w-8 h-8"/>
-                <span className="font-bold">Sign in</span>
+              <Link href='/signin' className="flex items-center header-button">
+              Hello, Sign in
               </Link>
               
               <Link href='/cart' className="header-button">
-               <ShoppingCartIcon className="w-8 h-8"/>
-                <span className="font-bold">Cart</span>              
+               <div className="flex items-end">
+                 <ShoppingCartIcon className="h-8 w-8"/>
+                </div>             
               </Link>
 
             </nav>
