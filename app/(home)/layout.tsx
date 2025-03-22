ed7f6718b9881551a668ff242a0824ffb70ec21e
mahedy25 <mahedyhasan873@gmail.com>
2025-03-22 22:33:21 +0600

    Navbar and footer created

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/components/ui/shared/footer"
+import Header from "@/components/ui/shared/header"
+
+
+export default async function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className='flex flex-col min-h-screen'>
+       <Header/> 
+       <main className='flex-1'>{children}</main>
+       <Footer />
+    </div>
+  )
+}
---
+++ lib/data.ts
@@ -0,0 +1,37 @@
+const data = {
+    headerMenus: [
+       {
+        name: "Today's Dael",
+        href: "/search?tag=todays-dael",
+       },
+       {
+        name: "New Arrivals",
+        href: "/search?tag=new-arrival",
+       },
+       {
+        name: "Featured Products",
+        href: "/search?tag=featured",
+       },
+       {
+        name: "Best Sellers",
+        href: "/search?tag=best-seller",
+       },
+       {
+        name: "Browsing History",
+        href: "/#browsing-history",
+       },
+       {
+        name: "Customer Service",
+        href: "/page/customer-service",
+       },
+       {
+        name: "About Us",
+        href: "/page/about-us",
+       },
+       {
+        name: "Help",
+        href: "/page/help",
+       },
+    ]
+}
+export default data
