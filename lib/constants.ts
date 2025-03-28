ac96b974b0bfb1e8d8a3a5c6ed627343c9baf861
mahedy25 <mahedyhasan873@gmail.com>
2025-03-28 20:35:06 +0600

    created product show page

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,5 @@ export const APP_SLOGAN =
     process.env.NEXT_PUBLIC_APP_SLOGAN || "BEST E-COMMERCE WEBSITE"
 export const APP_DESCRIPTION =
     process.env.NEXT_PUBLIC_APP_DESCRIPTION || "IT IS INSPIRED BY AMAZON.COM, CREATED BY NEXT JS & MONGODB"
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
