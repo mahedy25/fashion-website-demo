69d3566e86c52769633e15e0c25a117ed22c6393
mahedy25 <mahedyhasan873@gmail.com>
2025-04-09 17:15:55 +0600

    same

--- app/admin/products/product-form.tsx
+++ app/admin/products/product-form.tsx
@@ -161,7 +161,7 @@ const ProductForm = ({
     onClick={() => {
       form.setValue('slug', toSlug(form.getValues('name')))
     }}
-    className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
+    className="bg-blue-600 font-semibold text-white  px-4 py-1 rounded-md hover:bg-blue-700 transition"
   >
     Generate
   </button>
