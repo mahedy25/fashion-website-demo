3bc09a7591603f669973820eeba4da387cd2e824
mahedy25 <mahedyhasan873@gmail.com>
2025-04-11 00:05:00 +0600

    added user section for admin page

--- lib/db/actions/user.actions.ts
+++ lib/db/actions/user.actions.ts
@@ -5,9 +5,11 @@ import { UserSignUpSchema } from '@/lib/validator'
 import { IUserName, IUserSignIn, IUserSignUp } from '@/types'
 import { redirect } from 'next/navigation'
 import { connectToDatabase } from '..'
-import User from '../models/user.model'
+import User, { IUser } from '../models/user.model'
 import bcrypt from 'bcryptjs'
 import { formatError } from '@/lib/utils'
+import { revalidatePath } from 'next/cache'
+import { PAGE_SIZE } from '@/lib/constants'
 
 
 
@@ -61,5 +63,46 @@ export async function updateUserName(user: IUserName) {
     }
   } catch (error) {
     return { success: false, message: formatError(error) }
+  }
+}
+
+
+// DELETE
+
+export async function deleteUser(id: string) {
+  try {
+    await connectToDatabase()
+    const res = await User.findByIdAndDelete(id)
+    if (!res) throw new Error('Use not found')
+    revalidatePath('/admin/users')
+    return {
+      success: true,
+      message: 'User deleted successfully',
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+
+// GET
+export async function getAllUsers({
+  limit,
+  page,
+}: {
+  limit?: number
+  page: number
+}) {
+  limit = limit || PAGE_SIZE
+  await connectToDatabase()
+
+  const skipAmount = (Number(page) - 1) * limit
+  const users = await User.find()
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(limit)
+  const usersCount = await User.countDocuments()
+  return {
+    data: JSON.parse(JSON.stringify(users)) as IUser[],
+    totalPages: Math.ceil(usersCount / limit),
   }
 }
